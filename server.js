208e65bc431d20d0d813215581af52debc125239
Veronika Adelson <adelson1606@gmail.com>
2020-05-18 20:46:52 +0300

    basic server setups

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const api = require('./server/routes/api')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+
+// Mongoose setup
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/musicDB', { useNewUrlParser: true })
+
+app.use('/', api)
+
+const port = 3000
+app.listen(port, function () {
+  console.log(`Music running on port ${port}`)
+})
