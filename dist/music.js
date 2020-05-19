4938544ad9f6271626ebd9f66edd1c1c4201747c
Veronika Adelson <adelson1606@gmail.com>
2020-05-19 13:02:41 +0300

    change to make pretty

--- dist/music.js
+++ dist/music.js
@@ -1,19 +1,24 @@
 class musicApp {
 
-    constructor() {
-    }
+  constructor () {
+  }
 
     
 
-    async getSongData(song,singer) {
-           const songInfo = await $.get(`/music/?singer=${singer}&song=${song}`) 
-           if(songInfo) this.songData = songInfo
+  async getSongData (song, singer) {
+    const songInfo = await $.get(`/music/?singer=${singer}&song=${song}`) 
+    if (songInfo) {
+      this.songData = songInfo     
     }
 
-    async getTranslated(song,singer) {
-        const text = await $.get(`/translate/?singer=${singer}&song=${song}`) 
-        if(text) this.translatedText = text
- }
+  }
+
+  async getTranslated (song, singer) {
+    const text = await $.get(`/translate/?singer=${singer}&song=${song}`) 
+    if (text) {
+      this.translatedText = text
+    }
+  }
 
 
 }
