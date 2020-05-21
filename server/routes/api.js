42761691627c636beee370d7feec4c16edffaa08
Veronika Adelson <adelson1606@gmail.com>
2020-05-21 10:11:34 +0300

    ready

--- server/routes/api.js
+++ server/routes/api.js
@@ -111,7 +111,7 @@ router.get('/music/', async function (req, res) {
 
 
 router.get('/songs', async function (req, res) {
-  const category = req.query.category.toLowerCase()
+  const category = req.query.category
   const songs = await Music.find({ 'category': category })
 
   res.send(songs)
